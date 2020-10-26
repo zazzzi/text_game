bd1248bf37a4d4df62d86dfa5763996419f5a12d
Sebastian Zazzi <sebastian@zazzi.se>
2020-10-26 18:41:49 +0100

    Added more of the mage storyline

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 
+/**
+ * Array list, all inputs from the user and responses from the js are pushed in to here and then printed on page.  
+ */
 let listItem = [];
 
 
@@ -16,60 +19,79 @@ function startGame() {
 }
 
 /**
- * 
+ * Function that records the user inputs 
  * @param {User-Input} form 
  */
 function userInput(form) {
     textIn = form.inputField.value.toLowerCase();
-    listItem.push(textIn);
 
-    //#region If-satser 
 
-    //#region Mage
+    //#region All the different if-statements for the whole quesline
+
+    //#region Mage -- The mage questline starts here
+
     if (textIn == "mage") {
-        console.log("mage", listItem);
+        listItem.push(textIn);
         text = "Are you sure?"  //ADD INFO MAGE 
         let answerPromt = prompt(text).toLowerCase();
         if (answerPromt == 'no') {
             startGame();
         }
         else {
-            text = "You are a talented mage, sitting in a liberary, holding a tome of the arcane arts. <br/> Would you like to read the tome or explore the library?"
+            text = "You are a talented mage, sitting in a liberary, holding a tome of the arcane arts. <br/> Would you like to read the tome or explore the library?";
             listItem.push(text);
             respons();
         }
     }
     if (textIn == "read" && listItem.includes("mage")) {
-        text = "You sit down on a nearby chair. Open the tome and start reading about the wonders of ancient magics. <br/> While sitting down you realise that reading are for fucking loosers. You burn down the library with your fire magic and go to the local bordello. "
-        // VICTORY! 
-        
-    }
-
-
-    if (textIn == "yes" && listItem.includes("mage")) {
-        console.log("steget efter mage");
-        text = "kiss";
-        listItem.push(kiss)
+        listItem.push(textIn);
+        text = "You sit down on a nearby chair. Open the tome and start reading about the wonders of ancient magics. <br/> While sitting down you realise that reading are for fucking loosers. You burn down the library with your fire magic and go to the local bordello. <br/> <br/> VICTORY! Only nerds read, and you are a cool mage.";
+        listItem.push(text);
         respons();
+
+        // VICTORY! ADD VICTORY screen
+
+
     }
+    if (textIn == "explore" && listItem.includes("mage")) {
+        listItem.push(textIn);
+        text = "You explore the library. Huge bookcases towering around you. The halls of the library seem to endlessly go on. <br/> All of the sudden, a group of goblins show up, looking at you with their empty eyes. <br/> As a mage you realise that fighthig the goblins might result in damages to the library... <br/> Would you like to try to befriend the goblins or attack them?";
+        listItem.push(text);
+        respons();
 
-
+    }
+    if (textIn == "attack" && listItem.includes("explore")) {
+        listItem.push(textIn);
+        text = "You attack the goblins with all of your might. Charging up your most powerful spell, you drain power from the air around you. <br/>You combine the arts of frost magic and fire magic, your hands glowing with ice and flames and just when you're about to let this monstosity of a spell loose, you roll a natural 1... <br/>Your critical fail results in you slipping on a banana peel and breaking your neck on a nearby chair. <br/><br/> DEATH. Fighting is not always the answer and as a mage of the highest order you should know this.";
+        listItem.push(text);
+        respons();
+        // DEATH! 
+    }
+    if (textIn == "befriend" && listItem.includes("explore")) {
+        listItem.push(textIn);
+        text = "You clear your troat, *erhmm*. <br/> '- Greetings, short green ones, what brings you to this library?' <br/> The goblin closes to you looks at you and responds '- We are here to learn about the language of the javasqriptians!' <br/> '- Oh word?' you say as the goblins seems to ready their weapons for an attack. ' - Then all you need to know is that you can never use to many if-statements' <br/> Your words seems to hurt the golins, as if this is the dumbest thing they've ever heard. You reapet yourself. <br/> ' - I really mostly use anything except if statements in this project atleast' you scream at the goblins. Upon hearing this meta-statement the goblins all selfcombust and fall down in small piles of ash.<br/><br/> VICTORY. You have defeated the goblins using only your wits, this is for sure the pinacle of you career as a mage.";
+        listItem.push(text); 
+        respons();
+        //Victory! 
+    }
 
     //#endregion
 
-    //#region Barbarian
+    //#region Barbarian -- Barb quesline starts here
     else if (textIn == "barbarian") {
         console.log("barb")
         respons();
     }
+    
 
     //#endregion
 
-    //#region Rogue
+    //#region Rogue -- Rogue questline starts here 
     else if (textIn == "rogue" && !listItem.includes("mage") && !listItem.includes("barbarian")) {
         console.log("rogue")
         respons();
     }
+ 
     //#endregion
 }
 //#endregion
@@ -82,6 +104,7 @@ function respons() {
     });
     document.getElementById("systemText").innerHTML = textPrint;
 }
+
 
 
 
